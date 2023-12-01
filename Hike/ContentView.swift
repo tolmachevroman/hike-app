9a165cb64d0608d689ec8f9e5db287d2bfa95e45
Roman Tolmachev <tolmachevroman@gmail.com>
2023-12-01 07:09:40 -0300

    initial project setup

---
+++ Hike/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by Roman Tolmachev on 1.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Roman Tolmachev on 1.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
