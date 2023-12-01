bbeece47ad01bc5562b16c61c1822991b31d9421
Roman Tolmachev <tolmachevroman@gmail.com>
2023-12-01 11:39:12 -0300

    add image change animation

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -8,6 +8,21 @@
 import SwiftUI
 
 struct CardView: View {
+    // MARK: - PROPERTIES
+    
+    @State private var imageNumber: Int = 1
+    @State private var randomNumber: Int = 1
+    
+    // MARK: - FUNCTIONS
+    
+    func randomImage() {
+        repeat {
+            randomNumber = Int.random(in: 1...5)
+        } while randomNumber == imageNumber
+        
+        imageNumber = randomNumber
+    }
+    
     var body: some View {
         // MARK: - CARD
         
@@ -54,16 +69,17 @@ struct CardView: View {
                         )
                         .frame(width: 256, height: 256)
                     
-                    Image("image-1")
+                    Image("image-\(imageNumber)")
                          .resizable()
-                     .scaledToFit()
+                         .scaledToFit()
+                         .animation(.default, value: imageNumber)
                 }
                 
                 // MARK: - FOOTER
                 
                 Button {
                     // ACTION: Generate a random number
-                    print("The button was pressed.")
+                    randomImage()
                 } label: {
                     Text("Explore More")
                         .font(.title2)
