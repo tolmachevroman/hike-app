d91bc1b891b69e87f969059252e4876d5fb2f77d
Roman Tolmachev <tolmachevroman@gmail.com>
2023-12-02 06:25:15 -0300

    add SettingsView

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -12,6 +12,7 @@ struct CardView: View {
     
     @State private var imageNumber: Int = 1
     @State private var randomNumber: Int = 1
+    @State private var isShowingSheet: Bool = false
     
     // MARK: - FUNCTIONS
     
@@ -45,9 +46,14 @@ struct CardView: View {
                         
                         Button {
                             // ACTION: Show a Sheet
-                            print("The button was pressed.")
+                            isShowingSheet.toggle()
                         } label: {
                             CustomButtonView()
+                        }
+                        .sheet(isPresented: $isShowingSheet) {
+                            SettingsView()
+                                .presentationDragIndicator(.visible)
+                                .presentationDetents([.medium, .large])
                         }
                     }
                     
